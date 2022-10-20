4ca889b409c8b83c7978aca5274d743b5cfb7e8b
Jeremy Jackson <jeremyorama@gmail.com>
2022-10-20 19:21:39 -0400

    Update update-dns.sh

--- update-dns.sh
+++ update-dns.sh
@@ -1,12 +1,15 @@
+interface='ens192'
 for (( i=1; i<=255; i++ ))
 do
     lookup=$(nslookup "host$i.k8.local")
     if [ $? -eq 1 ]
     then
         echo "host$i.k8.local does not exist"
-        ADDR=`/sbin/ifconfig ens192 | grep 'inet' | grep -v 'inet6'| awk '{print $2}'`
+        ADDR=$(ip address show dev $interface | grep 'inet' | grep -v 'inet6' | awk '{print $2}')
+        ADDR=${ADDR%%???}
         HOST="host$i.k8.local"
-        echo "update delete host$i.k8.local A" > /var/nsupdate.txt
+        echo "server 10.82.120.100 53" > /var/nsupdate.txt
+        echo "update delete host$i.k8.local A" >> /var/nsupdate.txt
         echo "update add host$i.k8.local 86400 A $ADDR" >> /var/nsupdate.txt
         echo "update delete host$i.k8.local PTR" >> /var/nsupdate.txt
         echo "update add host$i.k8.local 86400 PTR $ADDR" >> /var/nsupdate.txt
